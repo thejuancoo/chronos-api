22d328b4dec6d86516f67e1fe689de518092e8e0
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-01 13:36:46 -0600

    feat: Retorna los eventos

--- src/controller/eventController.js
+++ src/controller/eventController.js
@@ -9,7 +9,25 @@ export const getAllEvents = async (req, res) => {
             },
             raw: true
         })
-        res.json(events)
+
+        const calendar = events.reduce((acc, event) => {
+            const date = event.date_event
+
+            if(!acc[date]){
+                acc[date] = []
+            }
+
+            acc[date].push({
+                id: event.id_event,
+                title: event.title_event,
+                description: event.description_event,
+                time: event.time_event
+            })
+
+            return acc
+        }, {})
+
+        res.json(calendar)
     } catch (error) {
         console.log(error)
     }
