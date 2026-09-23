5a7d14f4cdbb76c41eabfa8ce9f04b1d0362ac73
Juan Cruz <jmcruzo23z@gmail.com>
2026-09-22 23:12:49 -0600

    feat: Agrega mejoras en la configuracion de la base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,16 @@ dotenv.config({path: '.env'})
 
 export const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     define: {
         timestamps: true
+    },
+    logging: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 })
